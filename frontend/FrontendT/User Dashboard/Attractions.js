0e2c4562d7aa4a05c318605b08a5c7b214e2a2bd
Tosa <anthonyfredericktosa@gmail.com>
2026-09-08 21:59:04 +0100

    fixed the sidebar routing

---
+++ frontend/FrontendT/User Dashboard/Attractions.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementsByClassName('sidebar');
+  const menuBtn = document.getElementById('menubtn');
+  const closeBtn = document.getElementById('closebtn');
+
+  // Mobile nav drawer
+  menuBtn.addEventListener('click', () => 
+  sidebar.classList.add('show-sidebar'));
+
+  closeBtn.addEventListener('click', () => 
+  sidebar.classList.remove('show-sidebar'));
+});
